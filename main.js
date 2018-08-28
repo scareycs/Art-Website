54e9622b99c4b18abd52ce11d18a9a33f4fc55d2
scareycs <scareycs@gmail.com>
2018-08-28 09:43:06 -0400

    menu functionality added

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
-'use strict';
+function scrollToAnchor(section){
+    var aTag = $("a[name='"+ section +"']");
+    $('html,body').animate({scrollTop: aTag.offset().top},'medium');
+}
+
+$("#home").click(function(){
+	scrollToAnchor('home');
+});
+
+$("#projects").click(function(){
+	scrollToAnchor('projects');
+});
+
+$("#about").click(function(){
+	scrollToAnchor('about');
+});
+
+$("#contact").click(function(){
+	scrollToAnchor('contact');
+});
+
+
+$(window).bind('scroll', function() {
+	var navHeight = $(window).height() - 80;
+	 if ($(window).scrollTop() > navHeight) {
+		$('nav').addClass('fixedTop');
+		$('nav').removeClass('fixedBottom');
+	 }
+	 else {
+	 	$('nav').addClass('fixedBottom');
+		$('nav').removeClass('fixedTop');
+	 }
+});
